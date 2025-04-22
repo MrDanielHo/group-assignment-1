a49466a2b8f56d91062a175fc8172dea38f0666f
Emma Acquah <emmaacquah@gmail.com>
2025-04-22 20:23:37 +0100

    Correct setup SQL syntax & add DB_PORT

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 
 const db = new Pool({
-    connectionString: process.env.DB_URL
+    connectionString: process.env.DB_URL,
+    port: process.env.DB_PORT || 5432
 })
 
 module.exports = db
