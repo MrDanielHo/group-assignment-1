c030a2a008f3ce67e2e33065dc4e644ad6872cb8
Shaun <shaunmadziva@gmail.com>
2025-04-23 12:18:01 +0100

    add post route for checking answers

--- server/routers/game.js
+++ server/routers/game.js
@@ -6,5 +6,7 @@ const gameRouter = Router();
 
 gameRouter.get("/", gameController.getAllGames);
 gameRouter.get("/:gameId/questions", gameController.getAllQuestionsByGame);
+gameRouter.post("/:questionId/checkAnswer", gameController.checkAnswer);
+
 
 module.exports = gameRouter;
